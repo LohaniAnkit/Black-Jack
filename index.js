62ba249f67f3238576fa5ea4c0d4a1bb7a227a8a
Ankit Raj Lohani <63522620+LohaniAnkit@users.noreply.github.com>
2024-04-03 16:51:47 +0530

    Update index.js
    updated the bug of  screen loading

--- index.js
+++ index.js
@@ -1,92 +1,74 @@
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-let sum = 0
+let sum = 0;
 
-let cards = []
+let cards = [];
 
-let hasBlackJack = false
-let isAlive = false
+let hasBlackJack = false;
+let isAlive = false;
 
-let message = ""
+let message = "";
 
 let player = {
-    name: "Per",
-    chips: 145
-}
+  name: "Per",
+  chips: 145,
+};
 
-let playerEl = document.getElementById("player-el")
-playerEl.textContent = player.name + ": $" + player.chips
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
 
-    let randomCard = Math.floor(Math.random()*13 ) + 1
-
-    if(randomCard === 1)
-        return 11
-
-    else if(randomCard === 11 || randomCard === 12 || randomCard === 13)
-        return 10
-
-    else
-        return randomCard
+  if (randomCard === 1) return 11;
+  else if (randomCard === 11 || randomCard === 12 || randomCard === 13)
+    return 10;
+  else return randomCard;
 }
 
 function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
 
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
+  cards = [firstCard, secondCard];
+  sum = cards[0] + cards[1];
 
-    cards = [firstCard, secondCard]
-    sum = cards[0] + cards[1]
+  isAlive = true;
+  renderGame();
 
-    isAlive = true
-    renderGame()
-
-    console.log(isAlive)
-    console.log(hasBlackJack)
-
+  console.log(isAlive);
+  console.log(hasBlackJack);
 }
 
 function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? ";
+  } else if (sum === 21) {
+    message = "You've got Blackjack! ";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! ";
+    isAlive = false;
+  }
 
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? "
-    } 
-    
-    else if (sum === 21) {
-        message = "You've got Blackjack! "
-        hasBlackJack = true
-    } 
-    
-    else {
-        message = "You're out of the game! "
-        isAlive = false
-    }
+  messageEl.innerText = message;
+  sumEl.textContent = "Sum: " + sum;
 
-    messageEl.innerText = message
-    sumEl.textContent = "Sum: " + sum
+  cardsEl.textContent = "Cards: ";
 
-    cardsEl.textContent = "Cards: "
-
-    for (let i = 0;i<cards.length;i++){
-        cardsEl.textContent += cards[i] + " "
-    }
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
 }
 
 function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    var newCard = getRandomCard();
+    cards.push(newCard);
 
-    if(isAlive === true && hasBlackJack === false){
-        var newCard = getRandomCard()
-        cards.push(newCard)
-
-        sum += newCard
-        renderGame()
-    }
-    
+    sum += newCard;
+    renderGame();
+  }
 }
-
-
-
-
